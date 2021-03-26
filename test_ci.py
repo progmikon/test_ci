ba383898a931dfc73c5e93b892127f07fd11b135
progmikon <mitin.kv>
2021-03-26 09:10:06 +0300

    First commit

--- test_ci.py
+++ test_ci.py
@@ -3,3 +3,6 @@ def sum_my(a, b):
 
 def multiply_my(a, b):
     return a * b
+
+def dev(a, b):
+    return a / b
--- tests/test_sum_my.py
+++ tests/test_sum_my.py
@@ -1,8 +1,16 @@
 import pytest
 
-from test_ci import sum_my
+from test_ci import sum_my, multiply_my, dev
 
 
 @pytest.mark.parametrize("a, b, result", [(1, 2, 3), (2, 3, 5)])
 def test_sum_my(a, b, result):
     assert sum_my(a, b) == result
+
+@pytest.mark.parametrize("a, b, result", [(1, 2, 2), (2, 3, 6)])
+def test_multiply(a, b, result):
+    assert multiply_my(a, b) == result
+
+@pytest.mark.parametrize("a, b, result", [(2, 1, 2), (6, 3, 2)])
+def test_dev(a, b, result):
+    assert dev(a, b) == result
