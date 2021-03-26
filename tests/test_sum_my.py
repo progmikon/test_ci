7613af36f714c6e83e081b8af12bb0e117471b24
progmikon <mitin.kv>
2021-03-26 08:10:01 +0300

    First commit

--- tests/test_sum_my.py
+++ tests/test_sum_my.py
@@ -3,6 +3,6 @@ import pytest
 from test_ci import sum_my
 
 
-@pytest.mark.parametrize("a, b, result", [(1, 2, 3), (2, 3, 4)])
+@pytest.mark.parametrize("a, b, result", [(1, 2, 3), (2, 3, 5)])
 def test_sum_my(a, b, result):
     assert sum_my(a, b) == result
